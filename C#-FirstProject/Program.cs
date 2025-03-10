cf45f341bf0833e051c8b984c50ca3513879a1d9
edine-noella <edinenoella@gmail.com>
2025-03-10 17:05:27 +0200

    chore: added greet a person functionality

--- C#-FirstProject/Program.cs
+++ C#-FirstProject/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("--------------------------------------");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "";
+        string greeting = "";
+        Console.WriteLine("Enter your name: ");
+
+        name = Console.ReadLine();
+
+        char[] charArray = { 'H', 'e', 'l', 'l', 'o' };
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            greeting += charArray[i];
+        }
+        Console.WriteLine(greeting + " " + name);
+    }
+}
+
+
